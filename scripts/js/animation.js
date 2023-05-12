95be057f9509c8abb9a9b0fbd0745446dd00c5ff
rezafaghih <royalgaming.work@gmail.com>
2023-05-12 21:01:10 +0430

    normal update

--- scripts/js/animation.js
+++ scripts/js/animation.js
@@ -1,3 +1,5 @@
+const inItem = []
+
 function importElementPoistion(el, max_content)
 {
   if (max_content == true)
@@ -8,10 +10,34 @@ function importElementPoistion(el, max_content)
   }
 }
 
+const items = document.querySelectorAll("[animation]");
 
-function processAnimation()
-{
-  elementPosition = importElementPoistion(element, true);
+
+function isItemInScreenZone(){
+  // chek html element position is between start of offset height and end of offset height
 }
 
 
+function processAnimation()
+{
+  var scrollTop = window.pageYOffset
+  for (let i =0; i<items.length; i++){
+    element_top = importElementPoistion(items[i], false);
+    if (scrollTop+window.innerHeight > element_top){
+        console.log(`more than ${i}`);
+    }
+  }
+}
+
+window.addEventListener("scroll", processAnimation)
+
+// processAnimation()
+
+
+
+async function fetchAnimation(){
+    const fetch = await fetch("../json/anim-property.json");
+    const json = await fetch.json();
+
+    return json;
+}
